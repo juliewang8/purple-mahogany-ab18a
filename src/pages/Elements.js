9107b786b182bbc6c0c0f671216f65275bc23dbb
Stackbit Code Editor <projects@stackbit.com>
2021-06-17 18:59:44 +0000

    Elements.js: changed

--- src/pages/Elements.js
+++ src/pages/Elements.js
@@ -309,8 +309,7 @@ const IndexPage = () => <Layout fullMenu>
 						<h4>Fit</h4>
 						<div className="box alt">
 							<div className="row gtr-uniform">
-								<div className="col-12"><span className="image fit"><img src={								<div className="col-12"><span className="image fit"><img src={https://i.imgur.com/kAz2mcZ.jpeg} alt="" /></span><h3>Amanda Kong</h3></div>
-} alt="" /></span><h3>Amanda Kong</h3></div>
+								<div className="col-12"><span className="image fit"><img src={pic8} alt="" /></span><h3>Amanda Kong</h3></div>
 								<div className="col-4"><span className="image fit"><img src={pic5} alt="" /></span><h3>Amanda Kong</h3></div>
 								<div className="col-4"><span className="image fit"><img src={pic6} alt="" /></span><h3>Amanda Kong</h3></div>
 								<div className="col-4"><span className="image fit"><img src={pic7} alt="" /></span><h3>Amanda Kong</h3></div>
