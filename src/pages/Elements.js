4e249271a67fcdbd78b0b189181430a8de3b008d
Stackbit Code Editor <projects@stackbit.com>
2021-06-17 13:43:50 +0000

    Elements.js: changed

--- src/pages/Elements.js
+++ src/pages/Elements.js
@@ -318,7 +318,7 @@ const IndexPage = () => <Layout fullMenu>
 								<div className="col-4"><span className="image fit"><img src={pic6} alt="" /></span><h3>Amanda Kong</h3></div>
 								<div className="col-4"><span className="image fit"><img src={pic6} alt="" /></span><h3>Amanda Kong</h3></div>
 								<div className="col-4"><span className="image fit"><img src={pic7} alt="" /></span><h3>Amanda Kong</h3></div>
-								<div className="col-4"><span className="image fit"><img src={pic5} alt="" /></span><<h3>Amanda Kong</h3>/div>
+								<div className="col-4"><span className="image fit"><img src={pic5} alt="" /></span><h3>Amanda Kong</h3></div>
 							</div>
 						</div>
 						<h4>Left &amp; Right</h4>
