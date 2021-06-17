53b6d37a18447a26221060f446f15d70c4cd05ad
Stackbit Code Editor <projects@stackbit.com>
2021-06-17 02:39:44 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   manifestName: 'SolidState',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
-  manifestBackgroundColor: '#ffffff',
+  manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
