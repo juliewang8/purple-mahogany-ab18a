8b83c325e3a537f3a5f09f7c44522545189d2368
Stackbit Code Editor <projects@stackbit.com>
2021-06-29 22:28:57 +0000

    SmallFooter.js: changed

--- src/components/SmallFooter.js
+++ src/components/SmallFooter.js
@@ -4,12 +4,13 @@ export default function SmallFooter() {
   return (
     <section id="footer">
       <div className="inner">
+      <p>Stay in the loop!</p>
       <ul className="icons">
         <li><a href="/#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
         <li><a href="/#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
         <li><a href="/#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
         <li><a href="/#" className="icon fa-github"><span className="label">Github</span></a></li>
-    </ul>   
+        </ul>   
         <ul className="copyright">
           <li>&copy; Solid State. All rights reserved.</li>
           <li>
