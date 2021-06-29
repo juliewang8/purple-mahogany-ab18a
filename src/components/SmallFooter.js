abb1ab8e2a0992b83d6393705c71dbf73a1a4adc
Stackbit Code Editor <projects@stackbit.com>
2021-06-29 22:31:50 +0000

    SmallFooter.js: changed

--- src/components/SmallFooter.js
+++ src/components/SmallFooter.js
@@ -8,7 +8,7 @@ export default function SmallFooter() {
       <ul className="icons">
         <li><a href="/#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
         <li><a href="/#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-        <li><a href="/#" className="icon fa-github"><span className="label">Github</span></a></li>
+        <li><a href="/#" className="icon fa-youtube"><span className="label">Youtube</span></a></li>
         </ul>   
         <ul className="copyright">
           <li>&copy; Solid State. All rights reserved.</li>
