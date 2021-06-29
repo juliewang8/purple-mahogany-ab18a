74805f6c9e9b4ad24f6aa6c65b4718513c556fca
Stackbit Code Editor <projects@stackbit.com>
2021-06-29 22:26:45 +0000

    SmallFooter.js: changed

--- src/components/SmallFooter.js
+++ src/components/SmallFooter.js
@@ -4,19 +4,12 @@ export default function SmallFooter() {
   return (
     <section id="footer">
       <div className="inner">
-        <ul className="contact">
-          <li className="fa-home"> </li>
-          <li className="fa-phone"> </li>
-
-          {config.socialLinks.map(social => {
-            const { icon, url } = social;
-            return (
-              <li className={`${icon}`} key={url}>
-                <a href={url}>{url}</a>
-              </li>
-            );
-          })}
-        </ul>
+      <ul className="icons">
+									<li><a href="/#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+									<li><a href="/#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+									<li><a href="/#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
+									<li><a href="/#" className="icon fa-github"><span className="label">Github</span></a></li>
+								</ul>
         <ul className="copyright">
           <li>&copy; Solid State. All rights reserved.</li>
           <li>
