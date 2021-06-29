a4bea70f845b511fbbce538351f26aa97e1e515e
Stackbit Code Editor <projects@stackbit.com>
2021-06-29 23:39:41 +0000

    Members.js: changed

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -20,7 +20,7 @@ const IndexPage = () => (
         <div className="inner">
           <h2 className="major">Current members</h2>
              <div className="box alt">
-                    <div className="row gtr-uniform">
+                    <div className="row gtr-uniform aln-center">
                         <div className="col-4"><span className="image fit"><img src={pic5} alt="" /></span><h3>Amanda Kong</h3></div>
                         <div className="col-4"><span className="image fit"><img src={pic6} alt="" /></span><h3>Amanda Kong</h3></div>
                         <div className="col-4"><span className="image fit"><img src={pic7} alt="" /></span><h3>Amanda Kong</h3></div>
