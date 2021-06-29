d23e79ef3d16b24e83c74afa3edbe61a21c31550
Stackbit Code Editor <projects@stackbit.com>
2021-06-29 22:33:40 +0000

    SmallFooter.js: changed

--- src/components/SmallFooter.js
+++ src/components/SmallFooter.js
@@ -6,7 +6,7 @@ export default function SmallFooter() {
       <div className="inner">
       <h3>Follow us!</h3>
       <ul className="icons">
-        <li><a href="/#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+        <li><a href="http://www.google.com" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
         <li><a href="/#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
         <li><a href="/#" className="icon fa-youtube"><span className="label">Youtube</span></a></li>
         </ul>   
