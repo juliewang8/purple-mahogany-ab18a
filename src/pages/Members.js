cfab58aaf39e8e2725fcf6f3981088a878a7c517
Stackbit Code Editor <projects@stackbit.com>
2021-06-29 22:53:40 +0000

    Members.js: changed

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -11,7 +11,6 @@ const IndexPage = () => (
       <header>
         <div className="inner">
           <h2>Meet the Seeds</h2>
-          <p>Phasellus non pulvinar erat. Fusce tincidunt nisl eget ipsum.</p>
         </div>
       </header>
 
