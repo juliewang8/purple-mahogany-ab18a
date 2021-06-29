bbbb771cff9ecc7a3b10b588d5bec12d443803b1
Stackbit Code Editor <projects@stackbit.com>
2021-06-29 23:51:20 +0000

    Members.js: changed

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -55,6 +55,7 @@ const IndexPage = () => (
                         <div className="col-4"><span className="image fit"><img src={pic6} alt="" /></span><h3>member</h3></div>
                         <div className="col-4"><span className="image fit"><img src={pic6} alt="" /></span><h3>member</h3></div>
             </div>
+            <h2 className="major"></h2>
         </div>
       </div>
       </div>
