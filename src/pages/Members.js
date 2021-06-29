f6a03ad4f1388f6c5b4894a524dc4d7aa94c3bcd
Stackbit Code Editor <projects@stackbit.com>
2021-06-29 22:16:33 +0000

    Members.js: changed

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import Layout from '../components/Layout';
+import PageLayout from '../components/PageLayout';
 import pic4 from '../assets/images/pic04.jpg';
 import pic5 from '../assets/images/pic05.jpg';
 
 const IndexPage = () => (
-  <Layout fullMenu>
+  <PageLayout fullMenu>
     <section id="wrapper">
       <header>
         <div className="inner">
@@ -80,7 +80,7 @@ const IndexPage = () => (
         </div>
       </div>
     </section>
-  </Layout>
+  </PageLayout>
 );
 
 export default IndexPage;
