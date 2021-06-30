2577cc04d5f9007e07784bbf10b68cc15cce4bae
Stackbit Code Editor <projects@stackbit.com>
2021-06-30 01:18:38 +0000

    Music.js: changed

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -48,7 +48,7 @@ const IndexPage = () => (
           </section>
          <h3 className="major">Repertoire</h3>
          <h4>Current</h4>
-         rain <br> i wanna know you <br>
+         rain <br> i wanna know you 
          <h4>Past</h4>
         </div>
       </div>
