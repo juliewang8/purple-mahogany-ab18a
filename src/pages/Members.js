20c5f88e91a156ead9dfe4f74a61115b47d228fb
Stackbit Code Editor <projects@stackbit.com>
2021-06-30 03:37:30 +0000

    Members.js: changed

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -22,17 +22,17 @@ const IndexPage = () => (
              <div className="box alt">
                     <div className="row gtr-uniform aln-center">
                         <div className="col-4"><span className="image fit"><img src={pic5} alt="" /></span>
-                        <h3 className="nopadding align-center">Jenna Riedl</h3><p className="align-center">PRESIDENT</p></div>
+                        <h3 className="nopadding align-center">Jenna Riedl</h3><p className="align-center">president</p></div>
                         <div className="col-4"><span className="image fit"><img src={pic6} alt="" /></span
                         ><h3 className="nopadding align-center">Jeremy Yeung</h3><p className="align-center">VP / MUSICAL DIRECTOR</p></div>
                         <div className="col-4"><span className="image fit"><img src={pic6} alt="" /></span
                         ><h3 className="nopadding align-center">Timothy Li</h3><p className="align-center">TREASURER</p></div>
                                                 <div className="col-4"><span className="image fit"><img src={pic6} alt="" /></span
-                        ><h3 className="nopadding align-center">Julia Chang</h3><p className="align-center">VP / MUSICAL DIRECTOR</p></div>
+                        ><h3 className="nopadding align-center">Julia Chang</h3><p className="align-center">SECRETARY</p></div>
                                                 <div className="col-4"><span className="image fit"><img src={pic6} alt="" /></span
-                        ><h3 className="nopadding align-center">Helen Cai</h3><p className="align-center">VP / MUSICAL DIRECTOR</p></div>
+                        ><h3 className="nopadding align-center">Helen Cai</h3><p className="align-center">INTERPERSONAL COORDINATOR</p></div>
                                                 <div className="col-4"><span className="image fit"><img src={pic6} alt="" /></span
-                        ><h3 className="nopadding align-center">Julie Wang</h3><p className="align-center">VP / MUSICAL DIRECTOR</p></div>
+                        ><h3 className="nopadding align-center">Julie Wang</h3><p className="align-center">PUBLIC RELATIONS CHAIR</p></div>
 
             </div>
 
