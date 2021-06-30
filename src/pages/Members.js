e0120939ea6156a5cd209046999cf3fd343e9a46
Stackbit Code Editor <projects@stackbit.com>
2021-06-30 03:46:33 +0000

    Members.js: changed

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -21,7 +21,9 @@ const IndexPage = () => (
           <h2 className="major align-center">Current members</h2>
              <div className="box alt">
                     <div className="row gtr-uniform aln-center">
-                        <div className="col-4"><span className="image fit"><img src={pic5} alt="" /></span>
+                        <div className="col-4"><span className="image fit"><img src={pic5} alt="" />
+                        </span>
+                        <div className="overlay"
                         <h3 className="nopadding align-center">Jenna Riedl</h3><p className="align-center">PRESIDENT</p></div>
                         <div className="col-4"><span className="image fit"><img src={pic6} alt="" /></span
                         ><h3 className="nopadding align-center">Jeremy Yeung</h3><p className="align-center">VP / MUSICAL DIRECTOR</p></div>
