94c6124709b798742798725892c45c72657e01f2
Stackbit Code Editor <projects@stackbit.com>
2021-07-23 18:49:28 +0000

    Music.js: changed

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -24,6 +24,7 @@ const IndexPage = () => (
           <h2 className="major">Listen to our music</h2>
           <p>
           <iframe width="560" height="315" src="https://www.youtube.com/embed/8GkZw-oxZdM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/YEdJiYG-6wQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </p>
 
           <section className="features">
