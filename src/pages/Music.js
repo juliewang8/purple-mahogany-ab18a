b3c04576a281f28c7198572932311e5648e097f0
Stackbit Code Editor <projects@stackbit.com>
2021-07-23 18:57:44 +0000

    Music.js: changed

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -18,7 +18,7 @@ const IndexPage = () => (
         <div className="inner">
           <h2 className="major">Mustard Seed Presents: metamorphosis</h2>
           <div>
-          <p> We are so proud to present our very first album! Since the group wasn't able to meet in person 
+          <p> We are so proud to present our very first album, Metamorphosis! Since the group wasn't able to meet in person 
           for the second half of the Spring '20 semester and the entirety of the Fall '20 semester, 
           we decided to record some of our songs and get them mixed into an album. Also included in the album
           are older recordings from Mustard Seed alumni that we got to release alongside our music! </p>
