4d259f0d409ff16e60cc34f89717a24f194889b4
Stackbit Code Editor <projects@stackbit.com>
2021-07-23 18:55:17 +0000

    Music.js: changed

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -20,7 +20,13 @@ const IndexPage = () => (
           <div>
           <p> We are so proud to present our very first album! Since the group wasn't able to meet in person 
           for the second half of the Spring '20 semester and the entirety of the Fall '20 semester, 
-          we decided to record some of our songs and get them mixed into an album!</p>
+          we decided to record some of our songs and get them mixed into an album. Also included in the album
+          are older recordings from Mustard Seed alumni that we got to release alongside our music! </p>
+          <br></br>
+          <br></br>
+          The name "Metamorphosis" is representative of the transformation that we have in Jesus Christ 
+          and is woven throughout the lyrics of the songs. From cries of desperation to songs of joy to declarations of 
+          our faith, Metamorphosis represents the journeys that we take as believers.
           <iframe src="https://open.spotify.com/embed/album/6dIzQjsSJbw8qI8mzzVe0l" width="100%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
            </div>
            <br></br>
