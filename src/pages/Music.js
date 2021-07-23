1f9c9500d27157a06670097ec6eb0bd17074290f
Stackbit Code Editor <projects@stackbit.com>
2021-07-23 18:52:18 +0000

    Music.js: changed

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -18,6 +18,7 @@ const IndexPage = () => (
         <div className="inner">
           <h2 className="major">Mustard Seed Presents: metamorphosis</h2>
           <div>
+          <p> </p>
           <iframe src="https://open.spotify.com/embed/album/6dIzQjsSJbw8qI8mzzVe0l" width="100%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
            </div>
            <br></br>
