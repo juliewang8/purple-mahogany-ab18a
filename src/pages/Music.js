a630b18688abdcc7fdf80ebd2e610bb9c6023a5a
Stackbit Code Editor <projects@stackbit.com>
2021-07-23 18:56:59 +0000

    Music.js: changed

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -24,7 +24,7 @@ const IndexPage = () => (
           are older recordings from Mustard Seed alumni that we got to release alongside our music! </p>
           <p>
           The name "Metamorphosis" is representative of the transformation that we have in Jesus Christ, a theme 
-          that is woven throughout the lyrics of the songs. From cries of desperation to shouts of joy, Metamorphosis represents the journeys that we take as believers.</p>
+          that is woven throughout the lyrics of these five songs. From cries of desperation to shouts of joy, Metamorphosis represents the journeys that we take as believers.</p>
 
           <iframe src="https://open.spotify.com/embed/album/6dIzQjsSJbw8qI8mzzVe0l" width="100%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
            </div>
