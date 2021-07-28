19cdc1db6c7087a8a6d49a23e03ec0257a7f75a4
Stackbit Code Editor <projects@stackbit.com>
2021-07-28 23:59:10 +0000

    Members.js: changed

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -25,7 +25,7 @@ const IndexPage = () => (
                         <div className="col-4">
                             <div className="image fit wrap">
                                 <img src="https://static.wixstatic.com/media/149ad6_ebcaca4e62af4086b22fb2d9578d70bd~mv2.jpg/v1/fill/w_286,h_280,al_c,q_80,usm_0.66_1.00_0.01/jenna_edited.webp" alt="" className="image member"/>
-                                    <div className="image fit description layer">
+                                    <div className="image fit descriptionlayer">
                                     <p class="image fit description">hello!</p></div>
                             </div>
 
