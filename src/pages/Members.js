d84ad6310fec70d0ee7779af763ad3b0a115c14e
Stackbit Code Editor <projects@stackbit.com>
2021-07-28 19:36:40 +0000

    Members.js: changed

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -27,7 +27,7 @@ const IndexPage = () => (
                         <p className="nopadding align-center">Jenna is a junior studying Painting, Art History, and Statistics at Boston University. 
                         On the rare occasions when she is not reading or making art, she can be found deep in the woods attempting to befriend squirrels.</p>
                         </div>
-                        <div className="col-4"><span className="image fit"><img src="https://static.wixstatic.com/media/149ad6_9e03af96c0884d7391a56de6b9f5bdf3~mv2.jpg/v1/crop/x_0,y_20,w_2000,h_1960/fill/w_286,h_280,al_c,q_80,usm_0.66_1.00_0.01/jeremy_edited.webp"alt="" /></span
+                        <div className="col-4"><span className="image fit"><img src="https://static.wixstatic.com/media/149ad6_9e03af96c0884d7391a56de6b9f5bdf3~mv2.jpg/v1/crop/x_0,y_20,w_2000,h_1960/fill/w_286,h_280,al_c,q_80,usm_0.66_1.00_0.01/jeremy_edited.webp" alt="" /></span
                         ><h3 className="nopadding align-center">Jeremy Yeung</h3><p className="align-center">VP / MUSICAL DIRECTOR</p></div>
                         <div className="col-4"><span className="image fit"><img src={pic6} alt="" /></span
                         ><h3 className="nopadding align-center">Timothy Li</h3><p className="align-center">TREASURER</p></div>
