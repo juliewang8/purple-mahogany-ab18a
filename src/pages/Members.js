2172dd6a807c301137f3a5b3c038d05f79185c57
Stackbit Code Editor <projects@stackbit.com>
2021-07-28 19:38:04 +0000

    Members.js: changed

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -31,7 +31,7 @@ const IndexPage = () => (
                         ><h3 className="nopadding align-center">Jeremy Yeung</h3><p className="align-center">VP / MUSICAL DIRECTOR</p></div>
                         <div className="col-4"><span className="image fit"><img src="https://static.wixstatic.com/media/149ad6_5781cd442b714590b69f1f6993f94950~mv2.jpg/v1/fill/w_286,h_280,al_c,q_80,usm_0.66_1.00_0.01/tim_edited.webp" alt="" /></span
                         ><h3 className="nopadding align-center">Timothy Li</h3><p className="align-center">TREASURER</p></div>
-                                                <div className="col-4"><span className="image fit"><img src={pic6} alt="" /></span
+                                                <div className="col-4"><span className="image fit"><img src="https://static.wixstatic.com/media/149ad6_bdf034fba9f34c13a5baa8dd56f79e10~mv2.jpg/v1/crop/x_472,y_152,w_1466,h_1466/fill/w_286,h_286,al_c,q_80,usm_0.66_1.00_0.01/juila_edited.webp" alt="" /></span
                         ><h3 className="nopadding align-center">Julia Chang</h3><p className="align-center">SECRETARY</p></div>
                                                 <div className="col-4"><span className="image fit"><img src={pic6} alt="" /></span
                         ><h3 className="nopadding align-center">Helen Cai</h3><p className="align-center">INTERPERSONAL COORDINATOR</p></div>
