aa1d49465f55a34b204e26b30ef10ece199c7476
Stackbit Code Editor <projects@stackbit.com>
2021-07-29 01:20:33 +0000

    LearnMore.js: changed

--- src/pages/LearnMore.js
+++ src/pages/LearnMore.js
@@ -25,10 +25,10 @@ const IndexPage = () => (
           <h3 className="major">Mission Statement</h3>
           <p className="no-padding">As one of the Christian a cappella groups in Boston, Mustard Seed hopes to share the love of Christ and the transformative power of the Holy Spirit in our lives through song. 
           </p>
-          <ul>
+          <ul className="no-padding">
           <li>To communicate Christ-centered messages of love and joy through a cappella.</li>
-          <li>To grow in our relationships with God and one another </li>
-          <li>To grow in our relationships with God and one another </li>
+          <li>To grow in our relationships with God and one another. </li>
+          <li>To respond to the needs of the church and greater Boston community. </li>
           </ul>
 
         </div>
