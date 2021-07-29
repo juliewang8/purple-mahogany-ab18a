27a5bef81117748f191095e2d4e3bda8b0e12581
Stackbit Code Editor <projects@stackbit.com>
2021-07-29 01:41:04 +0000

    FallAuditions.js: changed

--- src/pages/FallAuditions.js
+++ src/pages/FallAuditions.js
@@ -19,7 +19,6 @@ const IndexPage = () => (
           <p>
 
           We will be in person for the Fall 2021 Semester!! Auditions will be held at Boston University’s CFA building in September. The audition cycle typically consists of the initial audition where auditionees sing a verse and chorus of a song for the group, callbacks, and a social event. Dates and details are still yet to be determined but keep an eye out on our website and social media channels for more details as the Fall semester gets closer. Feel free to reach out to us with any questions and we look forward to hearing from you!
-
           </p>
 
           <h3 className="major yellow">More information</h3>
