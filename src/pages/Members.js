494a5e56f0fa5fc2c4af068e44909918beb1d9d3
Stackbit Code Editor <projects@stackbit.com>
2021-07-29 00:54:12 +0000

    Members.js: changed

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -96,7 +96,7 @@ const IndexPage = () => (
 
                         <div className="col-4">
                                 <div className="image fit wrap">
-                                    <img src="https://static.wixstatic.com/media/149ad6_f11f8e9aa45c47778aaa5f867b7af3fd~mv2.jpg/v1/crop/x_903,y_326,w_1768,h_1736/fill/w_286,h_280,al_c,q_80,usm_0.66_1.00_0.01/helen.webp"alt="" className="image fit img"/>
+                                    <img src="https://static.wixstatic.com/media/149ad6_cda1e56f658741c6a73f97e39434026f~mv2.jpg/v1/crop/x_0,y_20,w_2000,h_1960/fill/w_286,h_280,al_c,q_80,usm_0.66_1.00_0.01/julie_edited.webp"alt="" className="image fit img"/>
                                         <div className="image fit overlay overlay-primary">
                                             <div class="image fit title">title</div>
                                             <p class="image fit description">Jenna is a junior studying Painting, Art History, and Statistics at Boston University. 
