d5b77b2d7a6e9e463656d9a91246bd4dc6c36777
Stackbit Code Editor <projects@stackbit.com>
2021-07-29 01:49:22 +0000

    Events.js: changed

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -18,34 +18,54 @@ const IndexPage = () => (
         <div className="inner">
           <section className="features">
             <article>
-              <a href="/#" className="image">
+              <a href="/FallAuditions" className="image">
                 <img src={pic4} alt="" />
               </a>
-              <h3 className="major">Sed feugiat lorem</h3>
+              <h3 className="major">Fall 2021 auditions soon!</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id
-                nulla dignissim dapibus ultrices.
+                We will be in person for the Fall 2021 Semester!! Auditions will be held at Boston University’s CFA building in September. 
               </p>
-              <a href="/#" className="special">
+              <a href="/FallAuditions" className="special">
                 Learn more
               </a>
             </article>
             <article>
-              <a href="/#" className="image">
+              <a href="/SpringShowcase" className="image">
                 <img src={pic5} alt="" />
               </a>
-              <h3 className="major">Nisl placerat</h3>
+              <h3 className="major">Spring 2021 Showcase</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id
-                nulla dignissim dapibus ultrices.
+                Despite doing the Spring 2021 semester remotely, we had the opportunity to put on a virtual showcase!
               </p>
-              <a href="/#" className="special">
+              <a href="/SpringShowcase" className="special">
                 Learn more
               </a>
             </article>
             <article>
-              <a href="/#" className="image">
-                <img src={pic5} alt="" />
+              <a href="/OnlineBIDB" className="image">
+                <img src={pic6} alt="" />
+              </a>
+              <h3 className="major">Online BIDB 2021</h3>
+              <p>
+                Mustard Seed had the honor of virtually hosting the annual “Break it Down Boston” event, or BIDB, this past Spring!
+              </p>
+              <a href="/OnlineBIDB" className="special">
+                Learn more
+              </a>
+            </article>
+            <article>
+              <a href="/FCSConcert" className="image">
+                <img src={pic7} alt="" />
+              </a>
+              <h3 className="major">FCS A Cappella Benefit Concert</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id
+                nulla dignissim dapibus ultrices.
+              </p>
+              <a href="/FCSConcert" className="special">
+                Learn more
+              </a>
+            </article>
 
 
           </section>
