52664c858cba6b88ca9091638b5a675af82dcbfa
Stackbit Code Editor <projects@stackbit.com>
2021-07-29 01:38:14 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -90,7 +90,7 @@ The parable of the <i>mustard seed</i> is a story about a small dose of faith ac
               <a href="/#" className="image">
                 <img src={pic4} alt="" />
               </a>
-              <h3 className="major">Fall auditions soon!</h3>
+              <h3 className="major">Fall 2021 auditions soon!</h3>
               <p>
                 We will be in person for the Fall 2021 Semester!! Auditions will be held at Boston University’s CFA building in September. 
               </p>
