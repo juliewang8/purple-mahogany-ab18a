3eaee9d23228bfa26899db1d6f2dd3d90b36b87e
Stackbit Code Editor <projects@stackbit.com>
2021-07-29 01:44:30 +0000

    OnlineBIDB.js: changed

--- src/pages/OnlineBIDB.js
+++ src/pages/OnlineBIDB.js
@@ -20,7 +20,7 @@ const IndexPage = () => (
 Mustard Seed had the honor of virtually hosting the annual “Break it Down Boston” event, or BIDB, this past Spring! In a typical year, Christian a cappella groups from up and down the East Coast gather in Boston to perform and fellowship with one another. Because of the pandemic, we decided to go online. With around 70 attendees from our friends at Carnegie Mellon University, Johns Hopkins University, Rutgers University and many more, we had a blessed time of worship, games, and workshops. 
           </p>
           <p>
-          Check out the music, testimonies, and bad jokes HERE
+We were even able to put together the traditional group song: Check out “The Lord Bless You and Keep You” here. 
           </p>
         </div>
       </div>
