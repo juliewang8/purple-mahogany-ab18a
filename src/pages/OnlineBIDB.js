3f3abc770bf8c67cd4654435b3cd00ffa8241865
Stackbit Code Editor <projects@stackbit.com>
2021-07-29 01:44:02 +0000

    OnlineBIDB.js: changed

--- src/pages/OnlineBIDB.js
+++ src/pages/OnlineBIDB.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <section id="wrapper">
       <header>
         <div className="inner">
-          <h2>Spring 2021 Showcase</h2>
+          <h2>Virtual Break it Down Boston</h2>
         </div>
       </header>
 
