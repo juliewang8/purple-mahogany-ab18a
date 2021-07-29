2fa7e0278874e1ef48b9d41ed7e05fb85b0bb390
Stackbit Code Editor <projects@stackbit.com>
2021-07-29 01:46:06 +0000

    FCSConcert.js: changed

--- src/pages/FCSConcert.js
+++ src/pages/FCSConcert.js
@@ -17,7 +17,7 @@ const IndexPage = () => (
       <div className="wrapper">
         <div className="inner">
           <p>
-          Despite doing the Spring 2021 semester remotely, we had the opportunity to put on a virtual showcase! Our members spent the semester working hard to prepare mini projects in groups of two to four. A few members were also able to share testimonies about how God has been working in their lives, especially during this pandemic. Performances include songs from the Greatest Showman, HSMTMTS and by Pentatonix, Daniel Caesar, and Jon Bellion/Hillsong! 
+In the Spring 2021 semester, one of our group’s goal was to outreach to the community around us. So, we were honored to be invited by UC Berkeley’s For Christ’s Sake to participate in the Nationwide Christian A Cappella Benefit Concert. Our recording of “Brother x Deep Water”, a mashup by Kodaline and American Authors played at the show. In collaboration with a cappella groups all over the country, we were able to raise funds for various charitable foundations. Considering those in the area around us, Mustard Seed chose Boston Healthcare for the Homeless, who has been distributing COVID vaccines to the homeless and marginally house individuals. Praise the Lord we raised almost $150 for the organization. 
           </p>
           <p>Check out the music, testimonies, and bad jokes HERE</p>
         </div>
