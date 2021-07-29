c4fca11f029b057ab3ecf4b9e6a85d975a55e482
Stackbit Code Editor <projects@stackbit.com>
2021-07-29 01:46:02 +0000

    FCSConcert.js: changed

--- src/pages/FCSConcert.js
+++ src/pages/FCSConcert.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <section id="wrapper">
       <header>
         <div className="inner">
-          <h2>Spring 2021 Showcase</h2>
+          <h2>For Christ's Sake A Cappella Benefit Concert</h2>
         </div>
       </header>
 
