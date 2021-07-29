dfd85fce428e6db6459bc55aa4afbf32c863fd35
Stackbit Code Editor <projects@stackbit.com>
2021-07-29 01:39:04 +0000

    FallAuditions.js: changed

--- src/pages/FallAuditions.js
+++ src/pages/FallAuditions.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <section id="wrapper">
       <header>
         <div className="inner">
-          <h2>About us</h2>
+          <h2>Fall 2021 Auditions</h2>
         </div>
       </header>
 
