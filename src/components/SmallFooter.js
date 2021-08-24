92e76827019eaf343835f13b94519426d2fcf190
Stackbit Code Editor <projects@stackbit.com>
2021-08-24 23:25:16 +0000

    SmallFooter.js: changed

--- src/components/SmallFooter.js
+++ src/components/SmallFooter.js
@@ -6,7 +6,7 @@ export default function SmallFooter() {
       <div className="inner">
       <h3>Follow us!</h3>
       <ul className="icons">
-        <li><a href="http://www.google.com" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+        <li><a href="https://www.facebook.com/BUMustardSeedOfficial" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
         <li><a href="https://www.instagram.com/bu_mustardseed/" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
         <li><a href="/#" className="icon fa-youtube"><span className="label">Youtube</span></a></li>
         <li><a href="https://open.spotify.com/artist/1or0X6vMuuCJePhXVZsuzw?si=2UPRkssRT8ujlODwtVVaJA&dl_branch=1" className="icon fa-spotify"><span className="label">Spotify</span></a></li>
