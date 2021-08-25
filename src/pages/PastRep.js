f11d091d7b029eb204437690b5633b033345c5f2
Stackbit Code Editor <projects@stackbit.com>
2021-08-25 01:47:35 +0000

    PastRep.js: changed

--- src/pages/PastRep.js
+++ src/pages/PastRep.js
@@ -17,7 +17,7 @@ const IndexPage = () => (
       <div className="wrapper">
         <div className="inner">
           <h3 className="major">2020-2021</h3>
-                     <br/>
+                     Changed - Jordan <br/>
                      <br/>
                      <br/>
                      <br/>
