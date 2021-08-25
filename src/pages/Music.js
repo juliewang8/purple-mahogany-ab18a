91f9fc60e93ee856d7614ce54e49d869cfa3700d
Stackbit Code Editor <projects@stackbit.com>
2021-08-25 01:26:52 +0000

    Music.js: changed

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -37,6 +37,7 @@ const IndexPage = () => (
           <br></br>
           <br></br>
           <iframe width="560" height="315" src="https://www.youtube.com/embed/8GkZw-oxZdM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <br></br>
           <iframe width="560" height="315" src="https://www.youtube.com/embed/YEdJiYG-6wQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </p>
 
