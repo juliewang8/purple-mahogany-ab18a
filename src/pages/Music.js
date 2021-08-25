7a02715647495fd3fd89128dd6bc3415060af400
Stackbit Code Editor <projects@stackbit.com>
2021-08-25 01:25:03 +0000

    Music.js: changed

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -25,8 +25,6 @@ const IndexPage = () => (
           <p>
           The name <i>metamorphosis</i> is representative of the transformation that we have in Jesus Christ, a theme 
           that is woven throughout the lyrics of these five songs. From cries of desperation to shouts of joy, Metamorphosis represents the journeys that we take as believers.</p>
-
-          <iframe src="https://open.spotify.com/embed/album/6dIzQjsSJbw8qI8mzzVe0l" width="100%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
            </div>
            <br></br>
           <br></br>
