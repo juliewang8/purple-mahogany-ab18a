1a5ac8eaa6b7393a9ad139c3d2b339ddb594c232
Stackbit Code Editor <projects@stackbit.com>
2021-08-25 01:41:02 +0000

    PastRep.js: changed

--- src/pages/PastRep.js
+++ src/pages/PastRep.js
@@ -18,13 +18,6 @@ const IndexPage = () => (
         <div className="inner">
           <h3 className="major yellow">Why mustard seed?</h3>
           <h3 className="major yellow">Mission Statement</h3>
-          <p className="no-padding">As one of the Christian a cappella groups in Boston, Mustard Seed hopes to share the love of Christ and the transformative power of the Holy Spirit in our lives through song. 
-          </p>
-          <ul className="no-padding">
-          <li>To communicate Christ-centered messages of love and joy through a cappella.</li>
-          <li>To grow in our relationships with God and one another. </li>
-          <li>To respond to the needs of the church and greater Boston community. </li>
-          </ul>
 
         </div>
       </div>
