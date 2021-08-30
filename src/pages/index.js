77593bd15c599d5036bef70a368bbed1c112a02f
Stackbit Code Editor <projects@stackbit.com>
2021-08-30 21:32:40 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const IndexPage = () => (
     <section id="banner">
       <div className="inner">
         <div className="logo">
-          <span className="icon fa-music"></span>
+          <img src="https://static.wixstatic.com/media/149ad6_dbdf221a507d4096a26f5dd6131e7e05~mv2.jpg/v1/fill/w_2000,h_2000,al_c,q_85/149ad6_dbdf221a507d4096a26f5dd6131e7e05~mv2.webp" alt="" />
         </div>
         <h2>{config.heading}</h2>
         <p>{config.subHeading}</p>
