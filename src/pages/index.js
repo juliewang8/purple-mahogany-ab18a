98f75b5e6d2e8f9295015053e9f12bc1be6ee2e9
Stackbit Code Editor <projects@stackbit.com>
2021-08-30 21:27:25 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const IndexPage = () => (
     <section id="banner">
       <div className="inner">
         <div className="logo">
-          <span className="icon fa-heart-o"></span>
+          <span className="icon fa-tree"></span>
         </div>
         <h2>{config.heading}</h2>
         <p>{config.subHeading}</p>
