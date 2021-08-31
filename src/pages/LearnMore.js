ce280a67140cb2e2f720a7a620d255335c1411c3
Stackbit Code Editor <projects@stackbit.com>
2021-08-31 05:12:16 +0000

    LearnMore.js: changed

--- src/pages/LearnMore.js
+++ src/pages/LearnMore.js
@@ -16,7 +16,7 @@ const IndexPage = () => (
 
       <div className="wrapper">
         <div className="inner">
-        <img className="image" display= "block" margin-left="auto" margin-right="auto" height="auto" src="https://i.imgur.com/uzJcpNs.jpg" />
+        <img className="image" display= "block" height="auto" src="https://i.imgur.com/uzJcpNs.jpg" />
         <br></br>
         <br></br>
           <h3 className="major yellow">Why mustard seed?</h3>
