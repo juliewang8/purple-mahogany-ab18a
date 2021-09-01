191c4a9e16aaca141557cdd1d4b963c077a6c342
Stackbit Code Editor <projects@stackbit.com>
2021-09-01 17:43:40 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,6 +48,7 @@ The parable of the <i>mustard seed</i> is a story about a small dose of faith ac
       <section id="two" className="wrapper alt spotlight style2">
         <div className="inner">
           <a href="/Music" className="image">
+          <div>
             <img src="https://i.imgur.com/L6VN7Dt.jpg" alt="" />
           </a>
           <div className="content">
