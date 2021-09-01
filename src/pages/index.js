4ab7b5f104c859ee77a314785f2ca1f0454da6fd
Stackbit Code Editor <projects@stackbit.com>
2021-09-01 17:59:29 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ const IndexPage = () => (
   <Layout>
     <section id="banner">
       <div className="inner">
-         <div >
+         <div>
           <img border-radius="100%" overflow="hidden" src="https://i.imgur.com/VAm8oLG.png?1" alt="" />
         </div> 
         <h2>{config.heading}</h2>
