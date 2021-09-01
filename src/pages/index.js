a30073620a0691e502060d5a11b2b693d2f83159
Stackbit Code Editor <projects@stackbit.com>
2021-09-01 17:54:17 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ const IndexPage = () => (
     </section>
 
     <section id="wrapper">
-      <section id="one" className="wrapper spotlight style1">
+      <section id="one" className="">
         <div className="inner">
                 <a width="200px" height="200px" overflow="hidden" href="/LearnMore" className="image">
                     {/* <img src="https://static.wixstatic.com/media/149ad6_dbdf221a507d4096a26f5dd6131e7e05~mv2.jpg/v1/fill/w_2000,h_2000,al_c,q_85/149ad6_dbdf221a507d4096a26f5dd6131e7e05~mv2.webp" alt="" /> */}
