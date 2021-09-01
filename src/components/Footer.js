43daa5b12d027b383bb1c1328c53af45554ad02d
Stackbit Code Editor <projects@stackbit.com>
2021-09-01 18:49:21 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
 Whether you have questions about auditions, want to book Mustard Seed for an event, or if you just wanna chat, 
 feel free to shoot us an email or find us on our social medias!
         </p>
-        <form method="post" action="mailto:bumseed@gmail.com?subject=A message from your website!">
+        <form method="post" action="mailto:bumseed@gmail.com?subject=A message from your website!" enctype="multipart/form-data">
           <div className="fields">
             <div className="field">
               <label htmlFor="name">Name</label>
