0bd4e683be8b5cceadd2a7b2d2458ea4b496690a
Stackbit Code Editor <projects@stackbit.com>
2021-09-01 05:22:03 +0000

    Members.js: changed

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -76,7 +76,7 @@ const IndexPage = () => (
                                 </div>
 
                         <h3 className="nopadding align-center yellow">Julia Chang</h3><p className="lesspadding align-center">SECRETARY</p>
-                        <p className="nopadding align-center">Alto – BU'23</p> <p className="nopadding align-center">she/her | ISFP | Type 9</p>
+                        <p className="nopadding align-center">Alto – BU'23</p> <p className="nopadding align-center">she/her | ISFP | Type 9w1</p>
                         </div>
 
                         <div className="col-4">
