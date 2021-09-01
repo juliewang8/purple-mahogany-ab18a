793ca3febb9f3cc784033ce4631c75d94c3f0780
Stackbit Code Editor <projects@stackbit.com>
2021-09-01 18:28:43 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ const IndexPage = () => (
   <Layout>
     <section id="banner">
       <div className="inner">
-         <div style={{width: "100px", height: "100px"}}>
+         <div style={{width: "100px", height: "100px", opacity: ".5"}}>
           <img src="https://i.imgur.com/GShsA7c.png?1" alt="" />
         </div> 
         <br/>
