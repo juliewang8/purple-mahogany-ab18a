f3c95355c1e5c12d2a058d7f4debf403e515e2e7
Stackbit Code Editor <projects@stackbit.com>
2021-09-01 17:51:55 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const IndexPage = () => (
     <section id="banner">
       <div className="inner">
          <div border-radius="100%" className="logo">
-          <img src="https://i.imgur.com/VAm8oLG.png?1" alt="" />
+          <img border-radius="100%" src="https://i.imgur.com/VAm8oLG.png?1" alt="" />
         </div> 
         <h2>{config.heading}</h2>
         <p>{config.subHeading}</p>
