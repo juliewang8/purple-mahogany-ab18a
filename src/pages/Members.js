db3ad54262ae1b9c1a2e5d4da4a7b5c4a194714f
Stackbit Code Editor <projects@stackbit.com>
2022-01-03 22:35:24 +0000

    Members.js: changed

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -52,7 +52,7 @@ const IndexPage = () => (
 
                         <div className="col-4">
                                 <div className="image fit wrap">
-                                    <img src="https://static.wixstatic.com/media/149ad6_5781cd442b714590b69f1f6993f94950~mv2.jpg/v1/fill" alt="" className="image fit img"/>
+                                    <img src="https://static.wixstatic.com/media/149ad6_5781cd442b71" alt="" className="image fit img"/>
                                         <div className="image fit overlay overlay-primary">
                                             <div class="image fit title">title</div>
                                             <p class="image fit description">As a Boston University Finance major and Economics minor, his mind is in Boston yet his heart is in Hong Kong (figuratively speaking). He has an idiosyncratic set of hobbies that range from investing to working on a start-up to cooking Bo Zai Fan and even perfecting his behind-the-back Billiards cue shot.
