06e2ff5102b25aa7dcc037c5d9ffadd7b46ee0a1
Stackbit Code Editor <projects@stackbit.com>
2022-01-04 02:18:36 +0000

    Members.js: changed

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -211,7 +211,7 @@ const IndexPage = () => (
                                     <img src="https://i.imgur.com/x7BR8fv.jpg?1"alt="" className="image fit img"/>
                                         <div className="image fit overlay overlay-primary">
                                             <div class="image fit title">title</div>
-                                            <p class="image fit description">becks two favorite things in life are making tik tok dances with friends and his dog tinker. he is a psychology major hoping to go into social work to support and care for the lgbtq+ community! </p>
+                                            <p class="image fit description"></p>
                                         </div>
                                 </div>
 
